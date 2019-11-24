a44ee94fd9f7672575f1df9b65e0eec2c02a8777
Roger Lipscombe <roger@differentpla.net>
2019-11-24 14:32:57 +0000

    Add dialyxir dependency

--- mix.exs
+++ mix.exs
@@ -25,7 +25,9 @@ defmodule NimbleParsecExperiments.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:nimble_parsec, "~> 0.5.2"}
+      {:nimble_parsec, "~> 0.5.2"},
+
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
