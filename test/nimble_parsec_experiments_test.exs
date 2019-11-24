4bcf52fd5a012548e700ce34f3f4575fa5d95d81
Roger Lipscombe <roger@differentpla.net>
2019-11-24 14:19:28 +0000

    Remove unit tests

--- test/nimble_parsec_experiments_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NimbleParsecExperimentsTest do
-  use ExUnit.Case
-  doctest NimbleParsecExperiments
-
-  test "greets the world" do
-    assert NimbleParsecExperiments.hello() == :world
-  end
-end
